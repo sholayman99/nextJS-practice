f8b92eb7d7df5f2f8e469bbf2ef2ef8c7fc87d69
Md Sholayman <mdsholayman998@gmail.com>
2024-02-22 16:50:53 +0600

    understanding catch all segments

---
+++ route-practice/src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,11 @@
+export default function Docs({params}:{
+    params:{slug:string[]};
+}){
+    if(params.slug?.length ===2){
+        return <h1>Viewing Docs for  {params.slug[0]} and {params.slug[1]}</h1>
+    }
+    else if(params.slug?.length === 1){
+        return <h1>Viewing docs for {params.slug[0]}</h1>
+    }
+    return <h1>Docs Page</h1>
+}
